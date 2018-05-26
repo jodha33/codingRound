7c7d6f6b373c6c8209756976244cca092c660e24
Jodha <jodhasingh30@gmail.com>
2018-05-26 17:43:49 +0530

    Added Browser Utility class with getter and setter method for webdriver and created a sample test in flight booking test class

--- src/main/java/utilities/BrowserUtility.java
+++ src/main/java/utilities/BrowserUtility.java
@@ -1,5 +1,36 @@
 package utilities;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.sun.javafx.PlatformUtil;
+
 public class BrowserUtility {
 
+	private WebDriver driver;
+		
+	public void setDriver(){
+		setDriverPath();
+		driver=new ChromeDriver();
+	}
+	
+	public WebDriver getDriver(){
+		return driver;
+	}
+	
+    private void setDriverPath() {
+        if (PlatformUtil.isMac()) {
+            System.setProperty("webdriver.chrome.driver", "chromedriver");
+        }
+        if (PlatformUtil.isWindows()) {
+            System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        }
+        if (PlatformUtil.isLinux()) {
+            System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
+        }
+    }
+    
+    public void closeDriver(){
+    	driver.close();
+    }
 }
--- src/main/tests/testcases/FlightBookingTest.java
+++ src/main/tests/testcases/FlightBookingTest.java
@@ -1,5 +1,35 @@
 package testcases;
 
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import utilities.BrowserUtility;
+
 public class FlightBookingTest {
 
+	BrowserUtility browserUtility;
+	WebDriver driver;
+	public FlightBookingTest() {
+		browserUtility=new BrowserUtility();
+		
+	}
+	
+	@BeforeTest
+	public void beforeTestMethod(){
+		browserUtility.setDriver();
+		driver=browserUtility.getDriver();
+	}
+	
+	@Test
+	public void testThatResultsAppearForAOneWayJourney(){
+		driver.get("http://www.google.com");
+	}
+	
+	@AfterTest
+	public void afterTestMethod(){
+		browserUtility.closeDriver();
+	}
+	
 }
